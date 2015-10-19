82fb9785168cc33b77d55cacf5a0cb3522fa41a3
cchoirat <cchoirat@gmail.com>
2015-10-19 17:05:05 -0400

    Fixed JSON for wrappers

--- R/create-json.R
+++ R/create-json.R
@@ -6,7 +6,14 @@ library(jsonlite)
 z5normalgam <- znormalgam$new()
 z5normalgam$toJSON()
 
-zeliggammodels <- list(zelig5gammodels = list("normal.gam" = z5normalgam$ljson))
+zeligmodels <- list(zelig5models = list("normalgam" = z5normalgam$ljson))
 
-cat(jsonlite::toJSON(zeliggammodels, pretty = TRUE),
-    file = file.path("inst/JSON", "zelig5gammodels.json"))
+# cat(toJSON(zeligmodels, pretty = TRUE), file = file.path("tools", "zelig5models.json"))
+# file.copy(from = file.path("tools", "zelig5models.json"), to = file.path("inst", "JSON", "zelig5models.json"))
+
+cat(toJSON(zeligmodels, pretty = TRUE), "\n", file = file.path("zelig5models.json"))
+file.rename(from = file.path("zelig5models.json"), to = file.path("inst", "JSON", "zelig5models.json"))
+file.remove(file.path("zelig5models.json"))
+
+# cat(toJSON(zeligmodels, pretty = TRUE))
+# j <- jsonlite::fromJSON(txt = readLines(file.path("..", "/JSON", "/zelig5models.json")))
